716da93cc4712b040159b87e7b3a815389fb41b6
Frank Mathy <frankmathy@gmail.com>
2019-12-29 13:45:05 +0100

    Changed implementation to use express

--- functions/index.js
+++ functions/index.js
@@ -7,33 +7,35 @@ admin.initializeApp({
     databaseURL: "https://socialape-c6bc6.firebaseio.com"
 });
 
-  // // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello world!");
-});
+const express = require('express');
+const app = express();
 
-exports.getScreams = functions.https.onRequest((req, res) => {
-    admin.firestore().collection('screams').get()
+app.get('/screams', (req, res) => {
+    admin
+        .firestore()
+        .collection('screams')
+        .orderBy('createdAt', 'desc')
+        .get()
         .then(data => {
             let screams = [];
             data.forEach(doc => {
-                screams.push(doc.data());
+                screams.push({
+                    screamId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt
+                });
             })
             return res.json(screams);
         })
         .catch(err => console.error(err));
 });
 
-exports.createScream = functions.https.onRequest((req, res) => {
-    if(req.method !== 'POST') {
-        return res.status(400).json({ error: 'Method not allowed'});
-    }
+app.post('/scream', (req, res) => {
     const newScream = {
         body: req.body.body,
         userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     };
 
     admin.firestore()
@@ -47,3 +49,5 @@ exports.createScream = functions.https.onRequest((req, res) => {
             console.error(err); 
         })
 });
+
+exports.api = functions.https.onRequest(app);
