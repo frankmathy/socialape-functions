24e6bd744d5923c07c3668b5d2101c49b9fcfe82
Frank Mathy <frankmathy@gmail.com>
2019-12-30 20:49:54 +0100

    Get user details

--- functions/dbschema.js
+++ functions/dbschema.js
@@ -1,4 +1,16 @@
 let db = {
+    users: [
+        {
+            userId: 'dh23ggjSh',
+            email: 'user@email.com',
+            handle: 'user',
+            createdAt: '2019-12-29T12:38:59.102Z',
+            imageUrl: 'image/djsdhjshd/skhjdksjd',
+            bio: 'Hi I am a develoer',
+            website: 'http://www.user.com',
+            location:'Wiesbaden, Germany'
+        }
+    ],
     screams: [
         {
             userHandle: 'user',
@@ -6,6 +18,16 @@ let db = {
             createdAt: '2019-12-29T12:38:59.102Z',
             likeCount: 5,
             commentCount: 2
+        }
+    ],
+    likes: [
+        {
+            userHandle: 'user',
+            screamId: 'shdjshduzs'
+        },
+        {
+            userHandle: 'user',
+            screamId: 'euzrujhdsdo'
         }
     ]
 }
