156039a8270f2cf84f36a9deec36ce6538d2c24d
Frank Mathy <frankmathy@gmail.com>
2020-01-03 00:00:51 +0100

    Fixed bug

--- functions/util/validators.js
+++ functions/util/validators.js
@@ -1,65 +1,65 @@
-const isEmpty = (text) => {
-    if(text.trim() === '') return true;
-    else return false;
-} 
+const isEmpty = text => {
+  if (text.trim() === "") return true;
+  else return false;
+};
 
-const isEmail = (email) => {
-    var regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if(email.toLowerCase().match(regEx)) return true;
-    else return false;
-}
+const isEmail = email => {
+  var regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.toLowerCase().match(regEx)) return true;
+  else return false;
+};
 
-exports.validateSignupData = (data) => {
-    let errors = {};
+exports.validateSignupData = data => {
+  let errors = {};
 
-    if(isEmpty(data.email)) {
-        errors.email = 'Must not be empty';
-    } else if(!isEmail(data.email)) {
-        errors.email = 'Must be a valid email address';
+  if (isEmpty(data.email)) {
+    errors.email = "Must not be empty";
+  } else if (!isEmail(data.email)) {
+    errors.email = "Must be a valid email address";
+  }
+
+  if (isEmpty(data.password)) {
+    errors.password = "Must not be empty";
+  }
+
+  if (data.password != data.confirmPassword) {
+    errors.confirmPassword = "Passwords must match";
+  }
+
+  if (isEmpty(data.handle)) {
+    errors.handle = "Must not be empty";
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length === 0 ? true : false
+  };
+};
+
+exports.validateLoginData = data => {
+  let errors = {};
+
+  if (isEmpty(data.email)) errors.email = "Must not be empty";
+  if (isEmpty(data.password)) errors.password = "Must not be empty";
+  console.log("Checked all values are ok");
+
+  return {
+    errors,
+    valid: Object.keys(errors).length === 0 ? true : false
+  };
+};
+
+exports.reduceUserDetails = data => {
+  let userDetails = {};
+  if (!isEmpty(data.bio.trim())) userDetails.bio = data.bio;
+  let website = data.website.trim();
+  if (!isEmpty(website)) {
+    if (website.substring(0, 4) != "http") {
+      userDetails.website = `http://${website}`;
+    } else {
+      userDetails.website = website;
     }
-
-    if(isEmpty(data.password)) {
-        errors.password = 'Must not be empty';
-    } 
-
-    if(data.password != data.confirmPassword) {
-        errors.confirmPassword = 'Passwords must match';
-    }
-
-    if(isEmpty(data.handle)) {
-        errors.handle = 'Must not be empty';
-    } 
-
-    return {
-        errors,
-        valid: Object.keys(errors).length === 0 ? true : false
-    }
-}
-
-exports.validateLoginData = (data) => {
-    let errors = {};
-
-    if(isEmpty(data.email)) errors.mail = "Must not be empty";
-    if(isEmpty(data.password)) errors.password = "Must not be empty";
-    console.log('Checked all values are ok')
-
-    return {
-        errors,
-        valid: Object.keys(errors).length === 0 ? true : false
-    }
-}
-
-exports.reduceUserDetails = (data) => {
-    let userDetails = {};
-    if(!isEmpty(data.bio.trim())) userDetails.bio = data.bio;
-    let website = data.website.trim();
-    if(!isEmpty(website)) {
-        if(website.substring(0,4) != 'http') {
-            userDetails.website = `http://${website}`;
-        } else {
-            userDetails.website = website;
-        }
-    }
-    if(!isEmpty(data.location.trim())) userDetails.location = data.location;
-    return userDetails;
-}
+  }
+  if (!isEmpty(data.location.trim())) userDetails.location = data.location;
+  return userDetails;
+};
