d780e25a85bfe6f2525b23d6fdce3412c9fdd1f0
Frank Mathy <frankmathy@gmail.com>
2020-01-03 01:31:53 +0100

    Fixed login response, reformatting

--- functions/dbschema.js
+++ functions/dbschema.js
@@ -1,33 +1,33 @@
 let db = {
-    users: [
-        {
-            userId: 'dh23ggjSh',
-            email: 'user@email.com',
-            handle: 'user',
-            createdAt: '2019-12-29T12:38:59.102Z',
-            imageUrl: 'image/djsdhjshd/skhjdksjd',
-            bio: 'Hi I am a develoer',
-            website: 'http://www.user.com',
-            location:'Wiesbaden, Germany'
-        }
-    ],
-    screams: [
-        {
-            userHandle: 'user',
-            body: 'this is the scream body',
-            createdAt: '2019-12-29T12:38:59.102Z',
-            likeCount: 5,
-            commentCount: 2
-        }
-    ],
-    likes: [
-        {
-            userHandle: 'user',
-            screamId: 'shdjshduzs'
-        },
-        {
-            userHandle: 'user',
-            screamId: 'euzrujhdsdo'
-        }
-    ]
-}
+  users: [
+    {
+      userId: "dh23ggjSh",
+      email: "user@email.com",
+      handle: "user",
+      createdAt: "2019-12-29T12:38:59.102Z",
+      imageUrl: "image/djsdhjshd/skhjdksjd",
+      bio: "Hi I am a develoer",
+      website: "http://www.user.com",
+      location: "Wiesbaden, Germany"
+    }
+  ],
+  screams: [
+    {
+      userHandle: "user",
+      body: "this is the scream body",
+      createdAt: "2019-12-29T12:38:59.102Z",
+      likeCount: 5,
+      commentCount: 2
+    }
+  ],
+  likes: [
+    {
+      userHandle: "user",
+      screamId: "shdjshduzs"
+    },
+    {
+      userHandle: "user",
+      screamId: "euzrujhdsdo"
+    }
+  ]
+};
--- functions/util/admin.js
+++ functions/util/admin.js
@@ -1,6 +1,6 @@
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 var serviceAccount = require("../socialapeKey.json");
-const config = require('./config');
+const config = require("./config");
 config.credential = admin.credential.cert(serviceAccount);
 admin.initializeApp(config);
 
